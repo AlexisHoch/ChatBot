6a1dc041a72d8b69417ca7e3b23a362aea9df292
AlexisHoch <37694448+AlexisHoch@users.noreply.github.com>
2018-12-06 21:45:35 +0100

    -Recherche intention fonctionnelle (basique)

--- JavaScript/test.js
+++ JavaScript/test.js
@@ -1,14 +1,31 @@
+var meteo = {
+  name : "meteo",
+  keyWords: ["meteo", "temps", "temperature"],
+  values: [10, 5, 5],
+  tree: [],
+  matchWords:0,
+};
 
-var tab = ["meteo", "temps", "temperature"];
+
+var intentions = [meteo];
 
 function getIntention (string) {
 
   var mots = string.split(' ');
-
   for(var i=0; i<mots.length; i++) {
-      console.log(mots[i]);
-  }
+      var mot=mots[i];
 
+      for(var j=0; j<intentions.length; j++){
+
+
+        for(var k =0; k<intentions[j].keyWords.length; k++){
+          if(intentions[j].keyWords[k]==mot){
+            intentions[j].matchWords=intentions[j].matchWords+intentions[j].values[k];
+            console.log(intentions[j].matchWords);
+          }
+        }
+      }
+  }
 
 
 
@@ -20,4 +37,4 @@ function getIntention (string) {
 
 
 //TESTS//
-getIntention("Je cherche une poupée russe");
+getIntention("Donne moi la meteo de demain");
